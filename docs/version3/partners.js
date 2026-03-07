3dec9830b445ad8b48d68c20463c59a54e1223e2
Kelly Speer <speerkelly@gmail.com>
2026-03-06 19:27:28 -0700

    Add files via upload

---
+++ docs/version3/partners.js
@@ -0,0 +1,110 @@
+window.BIOARC_PARTNERS = [
+  {
+    "id": "bvec",
+    "name": "Belize Vector Ecology Center",
+    "lat": 18.083,
+    "lon": -88.563
+  },
+  {
+    "id": "unemat",
+    "name": "Universidade do Estado de Mato Grosso (UNEMAT) — Reitoria (Cáceres, MT)",
+    "lat": -16.0567955,
+    "lon": -57.6702584
+  },
+  {
+    "id": "ufpr",
+    "name": "Universidade Federal do Paraná (Curitiba, PR)",
+    "lat": -25.424,
+    "lon": -49.2672
+  },
+  {
+    "id": "itps",
+    "name": "Instituto Todos pela Saúde (São Paulo, SP)",
+    "lat": -23.5592053,
+    "lon": -46.6585367
+  },
+  {
+    "id": "usp",
+    "name": "Universidade de São Paulo (Campus Butantã)",
+    "lat": -23.55972,
+    "lon": -46.72917
+  },
+  {
+    "id": "ciseal",
+    "name": "Centro de Investigación para la Salud en América Latina (CISeAL, PUCE – Nayón, Quito)",
+    "lat": -0.17636,
+    "lon": -78.42786
+  },
+  {
+    "id": "cunzac",
+    "name": "Centro Universitario de Zacapa – Universidad de San Carlos de Guatemala (CUNZAC)",
+    "lat": 14.9666766,
+    "lon": -89.5290792
+  },
+  {
+    "id": "uvg",
+    "name": "Universidad del Valle de Guatemala (UVG)",
+    "lat": 14.60492,
+    "lon": -90.48968
+  },
+  {
+    "id": "asich",
+    "name": "Asociación para la Sostenibilidad e Investigación Científica en Honduras (ASICH) — Barrio La Granja, Comayagüela",
+    "lat": 14.08048,
+    "lon": -87.21099
+  },
+  {
+    "id": "ocotal",
+    "name": "Colección Privada y Centro de Rescate de Fauna Silvestre El Ocotal (Sabanagrande)",
+    "lat": 13.79085,
+    "lon": -87.31298
+  },
+  {
+    "id": "udelar_fcien",
+    "name": "Facultad de Ciencias, Universidad de la República (Montevideo)",
+    "lat": -34.881332,
+    "lon": -56.1188972
+  },
+  {
+    "id": "iibce",
+    "name": "Instituto de Investigaciones Biológicas Clemente Estable (Montevideo)",
+    "lat": -34.8875445,
+    "lon": -56.1426497
+  },
+  {
+    "id": "unm",
+    "name": "University of New Mexico (Albuquerque)",
+    "lat": 35.0861284,
+    "lon": -106.6212218
+  },
+  {
+    "id": "umich",
+    "name": "University of Michigan (Ann Arbor)",
+    "lat": 42.2773704,
+    "lon": -83.707599
+  },
+  {
+    "id": "nau",
+    "name": "Northern Arizona University (Flagstaff)",
+    "lat": 35.1835619,
+    "lon": -111.6548145
+  },
+  {
+    "id": "nd",
+    "name": "University of Notre Dame (South Bend/Notre Dame, IN)",
+    "lat": 41.7035851,
+    "lon": -86.2337044
+  },
+  {
+    "id": "ou",
+    "name": "University of Oklahoma (Norman)",
+    "lat": 35.205894,
+    "lon": -97.445717
+  },
+  {
+    "id": "ku",
+    "name": "University of Kansas (Lawrence)",
+    "lat": 38.9541967,
+    "lon": -95.2597806
+  }
+];
